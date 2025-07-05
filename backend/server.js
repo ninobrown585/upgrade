cb5713863f9c5d9f4b9e412a1a8ec0a9b9d0e4df
Adrian Brown <ninobrown58518@gmail.com>
2025-07-05 14:45:52 -0500

    first official push

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import helmet from "helmet"; 
+import morgan from "morgan"; 
+import cors from "cors";
+import dotenv from "dotenv";
+import productRoutes from "./routes/productRoutes.js"
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet()); //This is a security middleware
+app.use(morgan("dev")); //logs the request
+
+app.use("/api/products", productRoutes)
+
+app.listen(PORT, () =>{
+    console.log("Server running on Port " + PORT);
+})
